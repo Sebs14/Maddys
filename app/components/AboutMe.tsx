69307df4436d5fb5914e27d8bdf73421e85d0e59
Sebs14 <00196919@uca.edu.sv>
2025-12-15 15:59:13 -0600

    refactor: simplify Lanyard component usage in AboutMe

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -6,10 +6,7 @@ const AboutMe = () => {
   return (
     <div className="h-screen w-full relative bg-[#d0fce3] flex justify-between items-center px-4 sm:px-8 md:px-12 lg:px-16 xl:px-20">
       <div className="w-full flex absolute left-0 -ml-40 sm:-ml-60 md:-ml-80 hidden sm:flex">
-        <Lanyard
-          position={[0, 0, 10]}
-          gravity={[0, -40, 0]}
-        />
+        <Lanyard position={[0, 0, 10]} gravity={[0, -40, 0]} />
       </div>
       <div className="flex items-center justify-center w-full sm:w-3/4 md:w-2/3 lg:w-1/2 h-full overflow-visible absolute top-0 right-0 sm:relative sm:ml-auto bg-transparent px-4">
         <Link
