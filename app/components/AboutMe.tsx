405354e3be8853230b19288d98f2313c223e64a7
Sebs14 <00196919@uca.edu.sv>
2025-12-15 15:21:26 -0600

    added new colors

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -4,11 +4,14 @@ import Link from "next/link";
 
 const AboutMe = () => {
   return (
-    <div className="h-screen w-full bg-[#aae8c5] flex justify-between items-center px-20">
-      <div className=" w-1/2 ">
-        <Lanyard position={[0, 0, 10]} gravity={[0, -40, 0]} />
+    <div className="h-screen w-full relative bg-[#d0fce3] flex justify-between items-center px-20">
+      <div className="w-full flex absolute left-0 -ml-80">
+        <Lanyard
+          position={[0, 0, 10]}
+          gravity={[0, -40, 0]}
+        />
       </div>
-      <div className="flex items-center justify-center w-1/2 h-full overflow-visible">
+      <div className="flex items-center justify-center w-1/2 h-full overflow-visible absolute top-0 right-0 bg-transparent">
         <Link
           href="/about"
           className=" font-figtree text-center text-4xl md:text-6xl lg:text-8xl font-bold text-[var(--color-orange)] hover:text-[var(--color-mint)] transition-colors duration-300"
--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${sora.variable} ${figtree.variable} antialiased`}
+        suppressHydrationWarning={true}
       >
         {children}
       </body>
